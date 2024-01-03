92b64f31a8325addb24af92392b93441fd4f94d2
Ben-Shan <bnshanahan@gmail.com>
2024-01-03 21:47:57 +0000

    Animation added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import SpacingSection from './components/Spacing/SpacingSection';
 import SizeSection from './components/Sizing/SizeSection';
 import ImagesSection from './components/Images/ImagesSection';
 import Guidance from './components/Guidance/GuidanceSection';
+import Animation from './components/Animation/AnimationSection';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <SizeSection/>
       <ImagesSection/>
       <Guidance/>
+      <Animation/>
     </div>
   );
 }
---
+++ src/components/Animation/AnimationSection.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './AnimationSection.css';
+
+function AnimationSection() {
+
+  return (
+    <div className="animation-container">
+      <div className="animation-title">
+        <h1>Animation</h1>
+      </div>
+      <p>
+      Animations or effects are used to draw attention and highlight importance. Subtle movements or visual enhancements can make an element more noticeable, guiding the user's focus and enriching their interactive experience.
+      </p>
+    </div>
+  );
+}
+
+export default AnimationSection;
